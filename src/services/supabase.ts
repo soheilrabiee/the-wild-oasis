7ba049e17ded4aa6112529b7e029a59df96af47c
Soheil <iamsoheil.r@gmail.com>
2025-10-06 00:39:27 +0330

    Add database types to the project

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,6 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
+import type { Database } from "../types/supabase";
+
 const supabaseUrl = "https://kpzrdbmultstvhsjmnwr.supabase.co";
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 
 export default supabase;
