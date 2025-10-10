34c450ff813d6b08d59ca453d10f3e46ba4801e7
Soheil <iamsoheil.r@gmail.com>
2025-10-10 22:17:49 +0330

    Add custom hook for updating setting data

--- src/services/apiSettings.ts
+++ src/services/apiSettings.ts
@@ -1,7 +1,7 @@
 import supabase from "./supabase";
 import type { Database } from "../types/supabase";
 
-type setting = Database["public"]["Tables"]["settings"]["Row"];
+type settingUpdate = Database["public"]["Tables"]["settings"]["Update"];
 
 export async function getSettings() {
     const { data, error } = await supabase
@@ -17,7 +17,7 @@ export async function getSettings() {
 }
 
 // We expect a newSetting object that looks like {setting: newValue}
-export async function updateSetting(newSetting: setting) {
+export async function updateSetting(newSetting: settingUpdate) {
     const { data, error } = await supabase
         .from("settings")
         .update(newSetting)
