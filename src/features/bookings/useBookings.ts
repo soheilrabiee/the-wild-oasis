2460323a481b82a88e92b2b8ea540e03418179bb
Soheil <iamsoheil.r@gmail.com>
2025-10-19 16:53:51 +0330

    Add api side filtering to the bookings page

--- src/features/bookings/useBookings.ts
+++ src/features/bookings/useBookings.ts
@@ -1,14 +1,28 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router";
 
 export function useBookings() {
+    const [searchParams] = useSearchParams();
+
+    // Filter
+    const filterValue = searchParams.get("status");
+    const filter =
+        !filterValue || filterValue === "all"
+            ? null
+            : {
+                  field: "status",
+                  value: filterValue,
+              };
+
     const {
         isPending,
         data: bookings,
         error,
     } = useQuery({
-        queryKey: ["bookings"],
-        queryFn: getBookings,
+        // Whenever the filter changes then the query refetches the data because the filter is in the dependency array
+        queryKey: ["bookings", filter],
+        queryFn: () => getBookings({ filter }),
     });
 
     return { isPending, bookings, error };
